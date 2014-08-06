5fba8c6919b0fcb4709edfa89ccfade690a96d4e
Esa Laine <esa@indoles.com>
2014-08-06 18:55:11 -0400

    pom additions for sonatype

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {:init-ns com.indoles.clj.mapdb-utils}
+  :pom-addition [:developers [:developer [:name "Indoles"]]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.mapdb/mapdb "1.0.5"]])
